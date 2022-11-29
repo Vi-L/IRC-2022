a743b205a1a1d508bc7fa19887769cd37a008da7
Vi-L <viclei1216@gmail.com>
2022-11-28 21:46:30 -0800

    add readme and clean up comments

---
+++ README.md
@@ -0,0 +1,8 @@
+# 2022 IRC Team Stripes Robot Code
+
+Robot code for auto, driving, and scoring 
+
+## TODOs:
+- Update Drivetrain.java VictorSP port numbers
+- Test driving, make sure everything works
+- Test auto, right now auto commands / durations are guesses
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,23 +6,14 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-// import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-/* TEMPLATE CODE
-//import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-//import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax; */
 
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.commands.Drive;
 import frc.robot.commands.BasicAuto;
 
 public class Robot extends TimedRobot {
-  // private DifferentialDrive m_myRobot;
   private Joystick m_leftStick;
   private Joystick m_rightStick;
-
-  /* TEMPLATE CODE
-  // private final MotorController m_leftMotor = new PWMSparkMax(0);
-  // private final MotorController m_rightMotor = new PWMSparkMax(1); */
 
   Drivetrain drivetrain = new Drivetrain();
   Drive drive = new Drive(drivetrain, m_leftStick, m_rightStick);
@@ -34,7 +25,6 @@ public class Robot extends TimedRobot {
     result in both sides moving forward. Depending on how your robot's
     gearbox is constructed, you might have to invert the left side instead."
     m_rightMotor.setInverted(true); */
-  // m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
 
     m_leftStick = new Joystick(0); //USB PORTS
     m_rightStick = new Joystick(1);
@@ -42,7 +32,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    // m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
     drive.execute();
   }
 
