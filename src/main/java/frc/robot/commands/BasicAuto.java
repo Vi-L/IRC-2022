4910067220331988b7ca499eabe659c83b34c7d2
Vi-L <viclei1216@gmail.com>
2022-12-01 19:17:14 -0800

    add comment

--- src/main/java/frc/robot/commands/BasicAuto.java
+++ src/main/java/frc/robot/commands/BasicAuto.java
@@ -9,7 +9,7 @@ public class BasicAuto extends SequentialCommandGroup {
     
     public BasicAuto(Drivetrain dt) {
         drivetrain = dt;
-
+        // to turn, make one value positive and the other negative
         addCommands(
             new AutoDrive(drivetrain, -0.4, -0.4).withTimeout(2), // drive forwards for 2 seconds
             new AutoDrive(drivetrain, 0, 0) // stop
